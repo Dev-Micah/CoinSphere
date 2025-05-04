ee1a2aec45426cc75e6272f59a8ab0ae62678e31
Micah Nyabuto <atembamicah@gmail.com>
2025-05-04 22:40:40 +0300

    Updated dependencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -61,6 +61,9 @@ dependencies {
     debugImplementation(libs.androidx.ui.tooling)
     debugImplementation(libs.androidx.ui.test.manifest)
 
+    //Coil
+    implementation("io.coil-kt:coil-compose:2.7.0")
+
     //SplashScreen
     implementation ("androidx.core:core-splashscreen:1.0.1")
 
@@ -84,6 +87,13 @@ dependencies {
     ksp("androidx.hilt:hilt-compiler:1.0.0")
     implementation("com.google.dagger:hilt-android:2.48")
     implementation("androidx.hilt:hilt-navigation-compose:1.1.0-alpha01")
+
+    //Retrofit
+    implementation ("androidx.lifecycle:lifecycle-viewmodel-compose:2.6.1")
+
+    implementation ("com.squareup.retrofit2:retrofit:2.9.0")
+    implementation ("com.squareup.retrofit2:converter-gson:2.9.0")
+    implementation ("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.7.1")
 
 
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.10.2")
