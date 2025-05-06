f569af58f8b15318b4c29660b71cd9439555c600
Micah Nyabuto <atembamicah@gmail.com>
2025-05-06 14:27:31 +0300

    Added RepositoryImpl

--- app/src/main/java/com/micahnyabuto/coinsphere/data/repository/CoinSphereRepositoryImpl.kt
+++ app/src/main/java/com/micahnyabuto/coinsphere/data/repository/CoinSphereRepositoryImpl.kt
@@ -2,11 +2,10 @@ package com.micahnyabuto.coinsphere.data.repository
 
 import com.micahnyabuto.coinsphere.data.remote.Coin
 import com.micahnyabuto.coinsphere.data.remote.CoinSphereApiService
-import javax.inject.Inject
 
-class CoinSphereRepository(
+class CoinSphereRepositoryImpl(
     private val coinSphereApiService:CoinSphereApiService
-) {
+) : CoinSphereRepository{
 
     override suspend fun getCoins(): List<Coin>{
         return coinSphereApiService.getCoins()
