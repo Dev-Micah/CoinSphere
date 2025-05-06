3c9455fd8db0aea7b5b3d76140e1e215ceff7f0d
Micah Nyabuto <atembamicah@gmail.com>
2025-05-06 14:30:12 +0300

    Updated the BottomNavigation

--- app/src/main/java/com/micahnyabuto/coinsphere/ui/navigation/BottomNavigation.kt
+++ app/src/main/java/com/micahnyabuto/coinsphere/ui/navigation/BottomNavigation.kt
@@ -2,10 +2,12 @@ package com.micahnyabuto.coinsphere.ui.navigation
 
 import android.graphics.drawable.Icon
 import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Article
 import androidx.compose.material.icons.filled.BarChart
 import androidx.compose.material.icons.filled.Explore
 import androidx.compose.material.icons.filled.Search
 import androidx.compose.material.icons.filled.Settings
+import androidx.compose.material.icons.filled.Star
 import androidx.compose.ui.graphics.vector.ImageVector
 
 enum class BottomNavigation(
@@ -21,18 +23,26 @@ enum class BottomNavigation(
         unselectedIcon = Icons.Default.BarChart,
         route = Destinations.Market
     ),
+    News(
+        label = "News",
+        selectedIcon = Icons.Default.Article,
+        unselectedIcon = Icons.Default.Article,
+        route = Destinations.News
+    ),
+    Favourite(
+        label ="Favourites",
+        selectedIcon = Icons.Default.Star,
+        unselectedIcon = Icons.Default.Star,
+        route = Destinations.Favourite
+
+    ),
     Search(
         label ="Search",
         selectedIcon = Icons.Default.Search,
         unselectedIcon = Icons.Default.Search,
         route = Destinations.Search
     ),
-    Discover(
-        label = "Discover",
-        selectedIcon = Icons.Default.Explore,
-        unselectedIcon = Icons.Default.Explore,
-        route = Destinations.Discover
-    ),
+
     Settings(
         label = "Settings",
         selectedIcon = Icons.Default.Settings,
