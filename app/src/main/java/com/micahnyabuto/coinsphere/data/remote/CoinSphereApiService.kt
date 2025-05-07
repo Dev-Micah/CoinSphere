3c9f8057f9a25a2db3627140c0530fb9e27c2cb0
Micah Nyabuto <atembamicah@gmail.com>
2025-05-07 11:54:00 +0300

    Update

--- app/src/main/java/com/micahnyabuto/coinsphere/data/remote/CoinSphereApiService.kt
+++ app/src/main/java/com/micahnyabuto/coinsphere/data/remote/CoinSphereApiService.kt
@@ -8,7 +8,7 @@ interface CoinSphereApiService {
     suspend fun getCoins(
         @Query("vs_currency") currency: String = "usd",
         @Query("order") order: String = "market_cap_desc",
-        @Query("per_page") perPage: Int = 100,
+        @Query("per_page") perPage: Int = 150,
         @Query("page") page: Int = 1
     ): List<Coin>
 }
