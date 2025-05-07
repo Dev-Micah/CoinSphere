6a609177be2bd261182db7d48d44bc53c0362cd7
Micah Nyabuto <atembamicah@gmail.com>
2025-05-07 11:58:11 +0300

    Updated Viewmodel to hold SearchQuery

--- app/src/main/java/com/micahnyabuto/coinsphere/ui/screens/market/MarketViewModel.kt
+++ app/src/main/java/com/micahnyabuto/coinsphere/ui/screens/market/MarketViewModel.kt
@@ -6,8 +6,11 @@ import com.micahnyabuto.coinsphere.data.remote.Coin
 import com.micahnyabuto.coinsphere.data.repository.CoinSphereRepository
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.SharingStarted
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.combine
+import kotlinx.coroutines.flow.stateIn
 import kotlinx.coroutines.launch
 import javax.inject.Inject
 
@@ -18,12 +21,30 @@ class MarketViewModel@Inject constructor(
     private val _uiState = MutableStateFlow<UiState<List<Coin>>>(UiState.Loading)
     val uiState: StateFlow<UiState<List<Coin>>> = _uiState.asStateFlow()
 
+    private val _allCoins = MutableStateFlow<List<Coin>>(emptyList())
+
+
+    // Holds the current search query.
+    private val _searchQuery = MutableStateFlow("")
+    val searchQuery: StateFlow<String> = _searchQuery
+
+    // Combines the coin list and search query to produce a filtered list.
+    val filteredCoins: StateFlow<List<Coin>> = combine(_allCoins, _searchQuery) { coins, query ->
+        if (query.isBlank()) coins
+        else coins.filter { coin ->
+            coin.name.contains(query, ignoreCase = true) ||
+                    coin.symbol.contains(query, ignoreCase = true)
+        }
+    }.stateIn(viewModelScope, SharingStarted.WhileSubscribed(5000), emptyList())
+
+
     fun fetchCoins(){
         viewModelScope.launch {
 
             _uiState.value = UiState.Loading
             try {
             val coins =coinSphereRepository.getCoins()
+                _allCoins.value =coins
             _uiState.value = UiState.Success(coins)
             } catch (
                e: Exception
@@ -33,6 +54,11 @@ class MarketViewModel@Inject constructor(
             }
 
         }
+    }
+
+    fun onSearchQueryChange(query: String){
+        _searchQuery.value =query
+
     }
 
 
