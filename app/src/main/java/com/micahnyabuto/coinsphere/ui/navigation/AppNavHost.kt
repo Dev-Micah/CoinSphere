cc9433a89cabf457871a93c4aba42cbad59fb725
Micah Nyabuto <atembamicah@gmail.com>
2025-05-08 22:00:44 +0300

    Update

--- app/src/main/java/com/micahnyabuto/coinsphere/ui/navigation/AppNavHost.kt
+++ app/src/main/java/com/micahnyabuto/coinsphere/ui/navigation/AppNavHost.kt
@@ -34,7 +34,9 @@ fun AppNavHost(
             SearchScreen()
         }
         composable <Destinations.Settings>{
-            SettingsScreen()
+            SettingsScreen(
+                navController =navController
+            )
         }
 
      }
