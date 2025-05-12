80aeeea51c0c0f4c4b6991b1f0bbe99a7accccb7
Micah Nyabuto <atembamicah@gmail.com>
2025-05-12 17:01:51 +0300

    Added DetailsCreen route

--- app/src/main/java/com/micahnyabuto/coinsphere/ui/navigation/AppNavHost.kt
+++ app/src/main/java/com/micahnyabuto/coinsphere/ui/navigation/AppNavHost.kt
@@ -2,11 +2,16 @@ package com.micahnyabuto.coinsphere.ui.navigation
 
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
+import com.micahnyabuto.coinsphere.data.remote.Coin
+import com.micahnyabuto.coinsphere.ui.screens.details.CoinDetailsScreen
 import com.micahnyabuto.coinsphere.ui.screens.favourite.FavouritesScreen
+import com.micahnyabuto.coinsphere.ui.screens.favourite.FavouritesViewModel
 import com.micahnyabuto.coinsphere.ui.screens.market.MarketScreen
+import com.micahnyabuto.coinsphere.ui.screens.market.MarketViewModel
 import com.micahnyabuto.coinsphere.ui.screens.news.NewsScreen
 import com.micahnyabuto.coinsphere.ui.screens.search.SearchScreen
 import com.micahnyabuto.coinsphere.ui.screens.settings.SettingsScreen
@@ -16,27 +21,41 @@ fun AppNavHost(
     navController: NavHostController,
     modifier: Modifier = Modifier
 ){
+    val favouritesViewModel: FavouritesViewModel = hiltViewModel()
+    val marketViewModel: MarketViewModel = hiltViewModel()
+
     NavHost (
          modifier =modifier,
          navController = navController,
          startDestination = Destinations.Market
      ){
         composable <Destinations.Market>{
-            MarketScreen()
+            MarketScreen(
+                navController =navController
+            )
         }
         composable <Destinations.News>{
             NewsScreen()
         }
         composable <Destinations.Favourite>{
-            FavouritesScreen()
+            FavouritesScreen(
+                favouritesViewModel = favouritesViewModel,
+                marketViewModel = marketViewModel
+            )
         }
         composable <Destinations.Search>{
-            SearchScreen()
+            SearchScreen(
+                navController =navController
+            )
         }
         composable <Destinations.Settings>{
             SettingsScreen(
                 navController =navController
             )
+        }
+
+        composable <Destinations.Details>{
+            CoinDetailsScreen()
         }
 
      }
