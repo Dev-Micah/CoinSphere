85d48a8cae83ecca532f02b0e032133f27e256ad
Micah Nyabuto <atembamicah@gmail.com>
2025-05-12 17:03:11 +0300

    Update

--- app/src/main/java/com/micahnyabuto/coinsphere/ui/screens/favourite/FavouritesScreen.kt
+++ app/src/main/java/com/micahnyabuto/coinsphere/ui/screens/favourite/FavouritesScreen.kt
@@ -1,20 +1,69 @@
 package com.micahnyabuto.coinsphere.ui.screens.favourite
 
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.collectAsState
+import androidx.hilt.navigation.compose.hiltViewModel
+import androidx.lifecycle.viewmodel.compose.viewModel
+import com.micahnyabuto.coinsphere.ui.screens.market.CoinsRow
+import com.micahnyabuto.coinsphere.ui.screens.market.MarketViewModel
+import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import androidx.navigation.compose.rememberNavController
+import com.micahnyabuto.coinsphere.data.remote.Coin
 
 @Composable
 fun FavouritesScreen(
-    modifier: Modifier = Modifier
-){
-    Box(
-        modifier = Modifier.fillMaxSize(),
-        contentAlignment = Alignment.Center
-    ) {
-        Text("FavouritesScreen")
+    favouritesViewModel: FavouritesViewModel = hiltViewModel(),
+    marketViewModel: MarketViewModel = hiltViewModel()
+) {
+    val allCoins by marketViewModel.allCoins.collectAsState()
+    LaunchedEffect(allCoins) {
+        favouritesViewModel.setAllCoins(allCoins)
     }
+
+    FavouritesScreenContent()
+
+
+
+}
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun FavouritesScreenContent(
+    favouritesViewModel: FavouritesViewModel =hiltViewModel()
+){
+    val navController = rememberNavController()
+    val favouriteCoins by favouritesViewModel.favouriteCoins.collectAsState()
+    Scaffold (
+        topBar = {
+            TopAppBar(
+                title = {
+                    Text("Favourites")
+                }
+            )
+        }
+    ){
+        innerpadding ->
+        LazyColumn(
+            modifier = Modifier.padding(innerpadding),
+            verticalArrangement = Arrangement.spacedBy(16.dp)
+        ){
+            items(favouriteCoins){ coin ->
+                CoinsRow(
+                    coin =coin,
+                    navController = navController
+                )
+            }
+        }
+    }
+
 }
