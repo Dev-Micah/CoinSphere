d8854b666d2c1706a5dfc1259ceb01d7199e6033
Micah Nyabuto <atembamicah@gmail.com>
2025-06-07 22:19:32 +0300

    Update

--- app/src/main/java/com/micahnyabuto/coinsphere/ui/screens/settings/ThemePreference.kt
+++ app/src/main/java/com/micahnyabuto/coinsphere/ui/screens/settings/ThemePreference.kt
@@ -1,13 +1,11 @@
 package com.micahnyabuto.coinsphere.ui.screens.settings
 
 import android.content.Context
-import androidx.datastore.core.DataStore
 import androidx.datastore.preferences.core.booleanPreferencesKey
 import androidx.datastore.preferences.core.edit
 import androidx.datastore.preferences.preferencesDataStore
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.map
-import java.util.prefs.Preferences
 
 val Context.dataStore by preferencesDataStore(name = "settings")
 
@@ -24,7 +22,7 @@ object ThemePreference {
 
     fun getThemePreference(context: Context): Flow<Boolean> {
         return context.dataStore.data.map { prefs ->
-            prefs[THEME_KEY] ?: false
+            prefs[THEME_KEY] == true
         }
     }
 }
