2272aae93305c57248a2941cae122b63c2502d54
Micah Nyabuto <atembamicah@gmail.com>
2025-07-05 23:28:08 +0300

    Update

--- app/src/main/java/com/micahnyabuto/coinsphere/ui/navigation/Destinations.kt
+++ app/src/main/java/com/micahnyabuto/coinsphere/ui/navigation/Destinations.kt
@@ -1,36 +1,20 @@
 package com.micahnyabuto.coinsphere.ui.navigation
 
 import kotlinx.serialization.Serializable
+import okhttp3.Route
 
-sealed class Destinations {
-    @Serializable
-    object Splash
-    @Serializable
-    object Market
+sealed class Destinations (val route: String){
 
-    @Serializable
-    object SignUp
+    object Splash: Destinations("splash")
+    object Market: Destinations("market")
+    object SignUp: Destinations("signup")
+    object SignIn: Destinations("signin")
+    object Search: Destinations("search")
+    object News: Destinations("news")
+    object Favourite: Destinations("favourites")
+    object Settings: Destinations("settings")
+    object Details: Destinations("")
 
-    @Serializable
-    object SignIn
-
-
-    @Serializable
-    object Search
-
-    @Serializable
-    object News
-
-    @Serializable
-    object Favourite
-
-    @Serializable
-    object Settings
-
-    @Serializable
-    object Details
-
-    @Serializable
-    object Profile
+    object Profile: Destinations("profile")
 
 }
