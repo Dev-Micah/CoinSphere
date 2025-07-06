857cd8ce65a68429c65c544884f13a60ba7a68bd
Micah Nyabuto <atembamicah@gmail.com>
2025-07-06 12:10:19 +0300

    Updated destinations sealed class

--- app/src/main/java/com/micahnyabuto/coinsphere/ui/navigation/Destinations.kt
+++ app/src/main/java/com/micahnyabuto/coinsphere/ui/navigation/Destinations.kt
@@ -1,20 +1,19 @@
 package com.micahnyabuto.coinsphere.ui.navigation
 
-import kotlinx.serialization.Serializable
-import okhttp3.Route
-
 sealed class Destinations (val route: String){
 
     object Splash: Destinations("splash")
     object Market: Destinations("market")
     object SignUp: Destinations("signup")
-    object SignIn: Destinations("signin")
+    object SignIn: Destinations("login")
     object Search: Destinations("search")
     object News: Destinations("news")
     object Favourite: Destinations("favourites")
     object Settings: Destinations("settings")
-    object Details: Destinations("")
+    object Details: Destinations("coinDetails/{coinName}")
 
     object Profile: Destinations("profile")
+
+    fun detailsRoute (coinName: String) = "coinDetails/$coinName"
 
 }
