c4ada12d94ae52a4b078004811837427f8ebbac3
Dev-Micah <atembamicah@gmail.com>
2026-01-26 10:29:49 +0300

    Ktor migration

--- app/src/main/java/com/micahnyabuto/coinsphere/data/remote/CoinsApiService.kt
+++ app/src/main/java/com/micahnyabuto/coinsphere/data/remote/CoinsApiService.kt
@@ -1,15 +1,30 @@
 package com.micahnyabuto.coinsphere.data.remote
 
 import com.micahnyabuto.coinsphere.data.remote.dtos.CoinDto
-import retrofit2.http.GET
-import retrofit2.http.Query
+import io.ktor.client.HttpClient
+import io.ktor.client.call.body
+import io.ktor.client.request.get
+import io.ktor.client.request.parameter
+import io.ktor.serialization.JsonConvertException
 
-interface CoinsApiService {
-    @GET("coins/markets")
+class CoinsApiService(private val client: HttpClient) {
+
     suspend fun getCoins(
-        @Query("vs_currency") currency: String = "usd",
-        @Query("order") order: String = "market_cap_desc",
-        @Query("per_page") perPage: Int = 100,
-        @Query("page") page: Int = 1
-    ): List<CoinDto>
+        currency: String = "usd",
+        order: String = "market_cap_desc",
+        perPage: Int = 100,
+        page: Int = 1
+    ): List<CoinDto> {
+        return try {
+            client.get("coins/markets") {
+                parameter("vs_currency", currency)
+                parameter("order", order)
+                parameter("per_page", perPage)
+                parameter("page", page)
+            }.body()
+        } catch (e: JsonConvertException) {
+            println("Error parsing JSON: ${e.localizedMessage}")
+            emptyList()
+        }
+    }
 }
